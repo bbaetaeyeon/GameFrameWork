0e6ea5e96587081d2bb11697d255ea63315fc01d
starr <starrain0273@naver.com>
2018-12-12 17:58:05 +0900

    PP11.AbstractClass

--- GameFrameWork/PP01.HelloSDL/Enemy.cpp
+++ GameFrameWork/PP01.HelloSDL/Enemy.cpp
@@ -1,23 +1,19 @@
 #include "Enemy.h"
 
+Enemy::Enemy(const LoaderParams* pParams) :
+	SDLGameObject(pParams)
+{
+}
+void Enemy::draw()
+{
+	SDLGameObject::draw(); // we now use SDLGameObject
+}
 void Enemy::update()
 {
-	GameObject::update();
 	m_y += 1;
-	//m_x += 1;
+	m_x += 1;
 	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
-}
-void Enemy::load(int x, int y, int width, int height,
-	std::string textureID)
-{
-	GameObject::load(x, y, width, height, textureID);
-}
-
-void Enemy::draw(SDL_Renderer* pRenderer)
-{
-	GameObject::draw(pRenderer);
 }
 void Enemy::clean()
 {
-	m_x -= 1;
 }
--- GameFrameWork/PP01.HelloSDL/Game.h
+++ GameFrameWork/PP01.HelloSDL/Game.h
@@ -3,11 +3,11 @@
 #include "Enemy.h"
 #include <vector>
 #include "LoaderParams.h"
+
+
 class Game
 {
 public:
-	Game() {}
-	~Game() {}
 
 	bool init(const char* title, int xpos, int ypos,
 		int width, int height, bool fullscreen);
@@ -17,17 +17,36 @@ public:
 	void handleEvents();
 	void clean();
 	bool running() { return m_bRunning; }
+	SDL_Renderer* getRenderer() const { return m_pRenderer; }
+
+	static Game* Instance()
+	{
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new Game();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
 
 private:
+	Game();
+	static Game* s_pInstance;
+
+	bool m_bRunning = true;
+
+	//int m_currentFrame;
+
 	SDL_Window * m_pWindow = 0;
 	SDL_Renderer* m_pRenderer = 0;
 	std::vector<GameObject*> m_gameObjects;
-	bool m_bRunning = true;
 
-	int m_currentFrame;
-
-	GameObject* m_go;
+	/*GameObject* m_go;
 	GameObject* m_player;
-	GameObject* m_enemy;
+	GameObject* m_enemy;*/
 
 };
+
+
+
+typedef Game TheGame;
